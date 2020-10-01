e32b8ecc26904d3e56648ee249618f0cb69d309b
arjun <arjun.ks3323@gmail.com>
2020-10-01 22:35:30 +0530

    my last commit

--- Description.c
+++ Description.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
+#include<graphic.h>
+#include<conio.h>
 void main()
 {
+  clrscr();
   printf("WELCOME TO HACKTOBERFEST 2020!!!!!");
   printf("So let's get started!");
-  printf("I guess everybody is a noobie here"); 
+  printf("I guess everybody is a noobie here");
 }

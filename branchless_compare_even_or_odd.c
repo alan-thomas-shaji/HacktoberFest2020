82158fe6f5a44736d1af19cfe32b26fb0cd79e19
tamton-aquib <aquibjavedt007@gmail.com>
2022-10-28 21:48:54 +0530

    better formatting + little cleanup

--- branchless_compare_even_or_odd.c
+++ branchless_compare_even_or_odd.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int i;
+
     for (i = 0; i<10000; i++)
         /* print even or odd with branchless comparison */
-        (i%2==0) ? printf("%d is %s\n",i,"Even") : printf("%d is %s\n",i,"Odd");
+        (i%2==0) ? printf("%d is %s\n", i, "Even") : printf("%d is %s\n", i, "Odd");
 }
--- fcfs.c
+++ fcfs.c
@@ -1,34 +1,36 @@
-#include<stdio.h>
-void main()
-{
+#include <stdio.h>
+
+void main() {
     int i, j, n, head;
     float avg, sum;
     sum = 0;
     int trackpos[20], stime[20];
+
     printf("Enter number of tracks : ");
     scanf("%d", &n);
     printf("Enter position of header : ");
     scanf("%d", &head);
     printf("Enter position of tracks : ");
-    for(i=0; i<n; i++)
-        {
-            scanf("%d", &trackpos[i]);
-            stime[i] = head - trackpos[i];
 
-            if(stime[i] < 0)
-            {
-                stime[i]=trackpos[i]-head;
-            }
-            head = trackpos[i];
-            sum = sum + stime[i];
+    for(i=0; i<n; i++) {
+        scanf("%d", &trackpos[i]);
+        stime[i] = head - trackpos[i];
+
+        if(stime[i] < 0) {
+            stime[i] = trackpos[i]-head;
         }
+        head = trackpos[i];
+        sum = sum + stime[i];
+    }
+
     avg = sum/n;
     printf("\nTrack position\tDifference between tracks");
-    for (i=0; i<n; i++)
-        {
-            printf("\n%d\t\t%d",trackpos[i],stime[i]);
-        }
-    printf("\n\nTotal movements of header : %f ",sum);
-    printf("\nAverage of header movements : %f ",avg);
+
+    for (i=0; i<n; i++) {
+        printf("\n%d\t\t%d", trackpos[i], stime[i]);
+    }
+
+    printf("\n\nTotal movements of header : %f ", sum);
+    printf("\nAverage of header movements : %f ", avg);
     printf("\n");
 }
